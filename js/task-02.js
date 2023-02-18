d846c4337d274b421bd97ed028b2908ab1c24950
Valentine <strheadshot1997@gmail.com>
2023-02-18 11:51:16 +0200

    edit work files

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemsList = document.querySelector("#ingredients");
+
+const ingredientsListEl = ingredients.map(ingredient => {
+  const listItemEl = document.createElement("li");
+  listItemEl.classList.add("item");
+  listItemEl.textContent = ingredient;
+
+  return listItemEl;
+});
+  
+
+itemsList.appendChild(ingredientsListEl);
