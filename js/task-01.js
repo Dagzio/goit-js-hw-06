2de6e37c6d7df98863711bbf4f99e88fdc5e99e1
Valentine Pylypchuk <strheadshot1997@gmail.com>
2023-02-27 14:26:41 +0200

    edit task01

--- js/task-01.js
+++ js/task-01.js
@@ -6,8 +6,8 @@ console.log("Number of categories: ", categoriesList.children.length);
 const categoriesListItems = categoriesList.querySelectorAll(".item");
 
 categoriesListItems.forEach(item => {
-    console.log("Category: ", item.firstElementChild.textContent)
+    console.log("Category: ", item.firstElementChild.textContent);
 
-    console.log("Elements: ", item.querySelectorAll("li").length)
+    console.log("Elements: ", item.lastElementChild.children.length);
 });
 
